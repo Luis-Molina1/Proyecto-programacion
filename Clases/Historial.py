d960a97728ff3387159849e6716f050e2b16d983
01k8z <mipavez24@alumnos.utalca.cl>
2026-04-28 18:14:49 -0400

    clase Historial

---
+++ Clases/Historial.py
@@ -0,0 +1,12 @@
+class Historial:
+    def _init_(self):
+        self_entradas=[]
+        self.limite=10
+    def agregar(self,url,titulo):
+        if self.entradas and self.entradas[1][0] == url:
+            return
+        self.entradas.append(url,titulo)
+        if len(self.entradas)>self.limite:
+            self.entradas.pop()
+    def obtener_historial(self):
+        return self.entradas
