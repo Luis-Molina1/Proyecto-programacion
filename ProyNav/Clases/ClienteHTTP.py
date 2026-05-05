57e0eba7ca0a774d93dd7ad3c66a1d13d037bf71
Luis-Molina1 <lmolina24@alumnos.utalca.cl>
2026-05-05 16:54:36 -0400

    clase ClienteHTTP

--- ProyNav/Clases/ClienteHTTP.py
+++ ProyNav/Clases/ClienteHTTP.py
@@ -5,34 +5,42 @@ import time
 
 class ClienteHTTP:
     def coneccion(self, url, segundos_retraso=3):
-        match = re.search(r"(https?)://([^/]+)", url)
+        match = re.search(r"(https?)://([^/]+)(.*)", url)
         if not match:
             print("URL inválida")
             return
-        protocol, dominio = match.groups()
+        protocol, dominio, ruta = match.groups()
         
-        port = 80
-        print(f"Conectando a {dominio} en puerto {port}...")
+        if not ruta:
+            ruta = "/"
+            
+        if protocol == "https":
+            port = 443
+            conn = http.client.HTTPSConnection(dominio, port, timeout=10)
+        else:
+            port = 80
+            conn = http.client.HTTPConnection(dominio, port, timeout=10)
+            
+        print(f"Conectando a {dominio} en puerto {port} con ruta {ruta}...")
         
         if segundos_retraso > 0:
             print(f"Esperando {segundos_retraso} segundos...")
             time.sleep(segundos_retraso)
             
-        conn = http.client.HTTPConnection(dominio, port, timeout=10)
-            
         try:
-            conn.request("GET", "/")
+            conn.request("GET", ruta)
             respuesta = conn.getresponse()
             
             print(f"Status: {respuesta.status} {respuesta.reason}")
-            print(respuesta.read().decode('utf-8', 'replace')[:300] + "...")
+            print(respuesta.read().decode('utf-8', 'replace') )
         except Exception as e:
             print(f"Error: {e}")
         finally:
             conn.close()
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     cliente = ClienteHTTP()
     
-    cliente.coneccion("http://utalca.cl")
+    cliente.coneccion("https://icc.utalca.cl")
     
     print("\nPrueba terminada.")
+"""
