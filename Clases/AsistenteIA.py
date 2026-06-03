9e9e3d20f6eec55c6d1d5b52fdd1316b5ebe6fd7
01k8z <mipavez24@alumnos.utalca.cl>
2026-06-02 23:22:10 -0400

    se implemento la conexion con la api de gemini

--- Clases/AsistenteIA.py
+++ Clases/AsistenteIA.py
@@ -2,17 +2,62 @@ import http.client
 import json
 
 class AsistenteIA:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.timeout = 10
-        self.dominio = "generativelanguage.googleapis.com"        
-    def procesar_comando(self):
-        ruta = f"/v1beta/interactions"
-        cuerpo = json.dumps({
+    API_KEY="AQ.Ab8RN6Lwa7u8PrRAC39FgvnTHHu87pmkLilzf2_8yzQY7f-Gbw"
+    def __init__(self, modelo="gemini-3.5-flash"):
+        self.timeout= 10
+        self.dominio= "generativelanguage.googleapis.com" 
+        self.modelo= modelo 
+
+    def procesar_comando(self, comando):
+        if not comando.strip():
+            print("comando vacio")
+            return None
+        
+        ruta= f"/v1beta/models/{self.modelo}:generateContent?key={self.API_KEY}"
+        prompt= f"responde en formato HTML y no pongas textualmente que estas respondiewndo en html. Pregunta: {comando}"
+        cuerpo= json.dumps({
             "contents": [
                 {
-                    "parts": [{"text": "w"}]
+                    "parts": [{"text": prompt}]
                 }
             ]
-            
         })  
+        try:
+            conn= http.client.HTTPSConnection(self.dominio, timeout= self.timeout)
+            conn.request("POST", ruta, body=cuerpo, headers={
+                "Content-Type": "application/json", 
+                "Host": self.dominio})
+            respuesta= conn.getresponse()
+            datos= respuesta.read().decode("utf-8")
+            if respuesta.status != 200:
+                print(f"Error: {respuesta.status} - {respuesta.reason}")
+                return None
+            respuesta= json.loads(datos)
+            texto = respuesta["candidates"][0]["content"]["parts"][0]["text"].strip()
+            if texto:
+                return texto
+            else:
+                print(f"gemini no genero respuesta")
+                return None
+        
+        except TimeoutError:
+            print(f"Error tiempo de espera agotado")
+            return None
+        except (KeyError, IndexError):
+            print(f"gemini no genero ninguna respuesta")
+            return None
+        except Exception as e:
+            print(f"error inesperado {e}")
+            return None
+        
+
+
+
+"""
+if __name__ == "__main__":
+    asistente = AsistenteIA()
+    pregunta = input("pregunta: ")
+    respuesta = asistente.procesar_comando(pregunta)
+    if respuesta:
+        print(f"respuesta: \n{respuesta}")
+"""
